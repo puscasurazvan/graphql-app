585544fdbf63fc85d60dcb6b47a07e83d601045c
Razvan Puscasu <puscasurazvan93@gmail.com>
2020-04-10 14:45:15 +0100

    graphql app

---
+++ graphql/resolvers/index.js
@@ -0,0 +1,17 @@
+const postsResolvers = require("./posts")
+const usersResolvers = require("./users")
+const commentsResolvers = require("./comments")
+
+module.exports = {
+  Query: {
+    ...postsResolvers.Query,
+  },
+  Mutation: {
+    ...usersResolvers.Mutation,
+    ...postsResolvers.Mutation,
+    ...commentsResolvers.Mutation,
+  },
+  // Subscription: {
+  //   ...postsResolvers.Subscription,
+  // },
+}
